66c30032932fc790c2799790e9cddeb88bf0e785
nikharju <n.harju@icloud.com>
2022-12-29 22:58:30 +0200

    Backup only - do not merge. Unstable.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-let numberOfPlayers = document.querySelector('#player-number-select')
-let form = document.querySelector('#player-select')
+let numberOfPlayers = 6 // document.querySelector('#player-number-select')
+//let form = document.querySelector('#player-select')
+const playerNames = []
 const playerData = []
 const scoreChart = []
 const playerIsComputer = []
@@ -7,10 +8,11 @@ let currentPlayer
 const dice = []
 const diceValues = [0,0,0,0,0,0]
 
-form.addEventListener('submit', e => {
+/* form.addEventListener('submit', e => {
     e.preventDefault()
     console.log(numberOfPlayers.value)
-} )
+} ) */
+
 function SetupPlayerNameEntryFields() {
     // Create number of text boxes based on selected number of players
     for (let i = 0; i < numberOfPlayers; i++) {
@@ -29,7 +31,7 @@ function SetupPlayerData() {
             // TODO: Get player names from text boxes
             playerData[i] = {playerName:playerNames[i], totalScore:0}
             // Create a score chart as an array of objects that is score cathegories. Array index = player (index 0 = player 1 etc.)
-            scoreChart[i] = {aces:0, twos:0, threes:0, fours:0, fives:0, sixes:0, bonus:0, onePair:0, twoPairs:0, threeOfAKind:0, fourOfAKind:0, smallStraight:0, largeStraight:0, fullHouse:0, chance:0, yahtzee:0}
+            scoreChart[i] = {aces:0, twos:0, threes:0, fours:0, fives:0, sixes:0, bonus:0, twoPairs:0, threeOfAKind:0, fourOfAKind:0, smallStraight:0, largeStraight:0, fullHouse:0, chance:0, yahtzee:0}
         }
     }
 }
@@ -66,15 +68,154 @@ function DiceArrayToValueArray(a, b, c, d, e) {
     diceValues[e-1] = diceValues[e-1]+1
 }
 
-function CalculatePoints() {
+function CheckDiceCategories() {
+    let diceCombinationStacks = [0, 0, 0, 0, 0, 0]
+    let stackCount = 0
+
+    diceValues.forEach (checkDice)
+    function checkDice (value, index, array) {
+        switch(value) {
+            case 2:
+                diceCombinationStacks[index] = diceCombinationStacks[index]+2
+                break
+            case 3:
+                diceCombinationStacks[index] = diceCombinationStacks[index]+3
+                break
+            case 4:
+                
+                break
+            case 5:
+
+                break
+            default:
+                console.log("He valt int ti na")
+        }
+    }
 
 }
+
+/* function CalculatePoints() {
+    // Check dice values and calculate score
+    let tempScore = 0
+    let diceCombinationStacks = [0, 0, 0, 0, 0, 0]
+    let stackCount = 0
+    let stackValueCount = 0
+
+    diceValues.forEach (checkDice)
+    function checkDice (value, index, array) {
+        
+        if (value == 2) {
+            diceCombinationStacks[index] = diceCombinationStacks[index]+2
+        }
+        if (value == 3) {
+            diceCombinationStacks[index] = diceCombinationStacks[index]+3
+        }
+        if (value == 4) {
+            console.log("FOUR OF A KIND")
+            diceValues.forEach (checkForFourOfAKind)
+            function checkForFourOfAKind (value, index, array) {
+                if (value == 4) {
+                    tempScore = tempScore + ((index+1)*value)
+                    scoreChart[currentPlayer].fourOfAKind = tempScore
+                }
+            }
+        }
+        if (value == 5) {
+            console.log("YAHTZEEEE!!!!")
+            diceValues.forEach (checkForYahtzee)
+            function checkForYahtzee (value, index, array) {
+                if (value == 5) {
+                    tempScore = tempScore + ((index+1)*value)
+                    scoreChart[currentPlayer].yahtzee = tempScore
+                }
+            }
+        }
+      }
+
+      diceCombinationStacks.forEach (countStacks)
+      function countStacks (value, index, array) {
+          if (value == 2) {
+              stackCount = stackCount+1
+              stackValueCount = stackValueCount+((index+1)*value)
+          }
+          if (value == 3) {
+              stackCount = stackCount+3
+              stackValueCount = stackValueCount+((index+1)*value)
+          }
+      }
+
+    if (stackCount == 2) {
+        console.log("Two Pairs!")
+        tempScore = tempScore + stackValueCount
+        scoreChart[currentPlayer].twoPairs = tempScore
+    } else if (stackCount == 4) {
+        console.log("FULL HOUSE!")
+        tempScore = tempScore + stackValueCount
+        scoreChart[currentPlayer].fullHouse = tempScore
+    } else if (stackCount == 1) {
+        console.log("Pair")
+        diceValues.forEach (checkForPair)
+        function checkForPair (value, index, array) {
+            if (value == 2) {
+                tempScore = tempScore + ((index+1)*value)
+                scoreChart[currentPlayer].onePair = tempScore
+            }
+        }
+    } else if (stackCount == 3) {
+        console.log("Three of a kind")
+        diceValues.forEach (checkForThreeOfAKind)
+        function checkForThreeOfAKind (value, index, array) {
+            if (value == 3) {
+                tempScore = tempScore + ((index+1)*value)
+                scoreChart[currentPlayer].threeOfAKind = tempScore
+            }
+        }
+    }
+
+    if (dice[4] == dice[3]+1 & dice[3] == dice[2]+1 & dice[2] == dice[1]+1 & dice[1] == dice[0]+1) {
+        if (dice[4] == 5) {
+            console.log("Small Straight")
+            tempScore = 15
+            scoreChart[currentPlayer].smallStraight = tempScore
+        } else if (dice[4] == 6) {
+            console.log("Large Straight")
+            tempScore = 20
+            scoreChart[currentPlayer].largeStraight = tempScore
+        }
+    }
+
+    if (tempScore == 0) {
+        console.log("Oh, that's a shame...")
+    } else {
+        players[currentPlayer].totalScore = players[currentPlayer].totalScore + tempScore
+    }
+
+
+     switch(scoreCategory) {
+          case aces:
+            // code block
+            break;
+          case twos:
+            // code block
+            break;
+          default:
+            // code block
+        } 
+} */
 
 function ShowFinalScore() {
 
 }
 
-/* ThrowDice()
+
+
+//Only for testing - remove the fillowing
+playerNames.push("Josh", "Andrew", "Phil", "Steve", "Motown", "Funk")
+SetupPlayerData()
+SetStartingPlayer()
+ThrowDice()
 console.table(dice)
 DiceArrayToValueArray(...dice)
-console.table(diceValues) */
+console.table(diceValues)
+//CalculatePoints()
+console.table(scoreChart)
