c3e8eee93908100cc43b0f0cff1e2beecd43c8ae
nikharju <n.harju@icloud.com>
2023-01-07 00:17:44 +0200

    Worked on misc functions. Maybe stable version, may merge maybe... baby

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-let numberOfPlayers = document.querySelector('#player-number-select')
+let numberOfPlayers = document.querySelector('#player-number-select') - 1 // Subtract 1 to convert to indexes starting at 0
 let form = document.querySelector('#player-select')
 const playerNames = []
 const playerData = []
 const scoreChart = []
-const playerIsComputer = []
+// const playerIsComputer = [] - moved to playerData
 let currentPlayer
 const dice = []
 const diceValues = [0,0,0,0,0,0]
+let throwCount = 0
+
+// Setup dice object array to default starting values
+for (let i = 0; i < 5; i++) {
+    dice[i] = {value:0, locked:false}
+}
 
 form.addEventListener('submit', e => {
     e.preventDefault()
@@ -21,28 +27,27 @@ function SetupPlayerNameEntryFields() {
 }
 
 function SetupPlayerData() {
-    if (numberOfPlayers > 6) {
-        return false
-    } else if (numberOfPlayers == 0) {
+    if (numberOfPlayers > 5) {
         return false
     } else {
-        for (let i = 0; i < numberOfPlayers; i++) {
+        for (let i = 0; i <= numberOfPlayers; i++) {
             // Create an array of objects (playerName and totalScore) with player name and set score to 0 for each player. Array index = player (index 0 = player 1 etc.)
-            // TODO: Get player names from text boxes
-            playerData[i] = {playerName:playerNames[i], totalScore:0}
+            // TODO: Get player names from text boxes, get playerIsComputer checkbox
+            playerData[i] = {playerName:playerNames[i], totalScore:0, playerIsComputer:false}
             // Create a score chart as an array of objects that is score cathegories. Array index = player (index 0 = player 1 etc.)
             scoreChart[i] = {aces:0, twos:0, threes:0, fours:0, fives:0, sixes:0, bonus:0, twoPairs:0, threeOfAKind:0, fourOfAKind:0, smallStraight:0, largeStraight:0, fullHouse:0, yahtzee:0, chance:0}
         }
+        SetStartingPlayer()
     }
 }
 
-function SetPlayerAsComputer() {
+/* function SetPlayerAsComputer() {             // moved to SetupPlayerData()
     // Option for user to set one or more players to be controlled by computer
     // TODO: Change the following temporary code for actual code. Now every playerIsComputer is false
     for (let i = 0; i < numberOfPlayers; i++) {
         playerIsComputer[i] = false
     }
-}
+} */
 
 function SetStartingPlayer() {
     // Randomly decide which player starts by setting a random player as first currentPlayer
@@ -50,16 +55,84 @@ function SetStartingPlayer() {
 }
 
 function ThrowDice() {
-    // Generate six random integers between 1-6 (individual dice points) as an array of five values (number of die)
-    for (let i = 0; i < 5; i++) {
-        dice[i] = (Math.floor(Math.random() * 6)+1)
-    }
+    throwCount++
+    console.log("Current player is " + currentPlayer + playerData[currentPlayer].playerName + " - throwCount: " + throwCount)
 
-    // Do we need to sort them in ascending order?
-    // dice.sort(function(a, b){return a - b})
+    // Generate five random integers between 1-6 (individual dice points) as an array of five values (number of dice)
+    for (let i = 0; i < 5; i++) {
+        if (dice[i].locked == false) {
+            dice[i].value = (Math.floor(Math.random() * 6)+1)
+        }
+    }
+    console.table(dice)
+    DiceArrayToValueArray(...dice.map(({ value }) => value))    // Convert dice to value array
+    //console.table(diceValues)
+    let resultArray = CheckThrowResults()                                         // Calculate results
+    PresentScoringOptions(resultArray)                                         // Present score category options to player
+}
+
+function PresentScoringOptions(array) {
+    // TODO: Get playerInput from UCI button
+    console.table(array)
+    let playerInput
+
+    if (throwCount >= 3) {           // Each player gets three throws per turn. If throwCount == 3 go to next player
+
+        // returns random key from Set or Map   ---   REMOVE ONCE USER INPUT IS COMPLETE - ONLY FOR TESTING
+        function getRandomKey(collection) {
+            let keys = Array.from(collection.keys());
+            return keys[Math.floor(Math.random() * keys.length)];
+        }
+        playerInput = getRandomKey(array)
+        //
+
+        let selectedScore = array.get(playerInput)
+
+        PlayerSelectScore(playerInput, selectedScore)
+    }
+}
+
+function PlayerSelectScore(playerSelection, score) {
+    // TODO: Needs fixing
+    console.log("Player selected " + playerSelection)
+    console.log("Score: " + score)
+    scoreChart[currentPlayer][playerSelection] = score
+
+    console.table(scoreChart)
+
+
+    NewPlayerTurn()
+}
+
+function NewPlayerTurn() {
+    throwCount = 0
+    if (currentPlayer < numberOfPlayers) {
+        currentPlayer++
+    } else if (currentPlayer == numberOfPlayers) {
+        currentPlayer = 0
+    }
+    UnlockAllDice()
+}
+
+function ToggleDieLocked(i) {
+    if (dice[i].locked == false) {
+        dice[i].locked = true
+    } else {
+        dice[i].locked = false
+    }
+}
+
+function UnlockAllDice() {
+    for (let i = 0; i < 5; i++) {
+        dice[i].locked = false
+    }
 }
 
 function DiceArrayToValueArray(a, b, c, d, e) {
+    // Reset old values
+    for (let i = 0; i < 6; i++) {
+        diceValues[i] = 0
+    }
     // Convert the array of dice to an array of dice values
     diceValues[a-1] = diceValues[a-1]+1
     diceValues[b-1] = diceValues[b-1]+1
@@ -70,13 +143,14 @@ function DiceArrayToValueArray(a, b, c, d, e) {
 
 function CheckThrowResults() {
     const diceCategoriesAndPoints = [["aces",0], ["twos",0], ["threes",0], ["fours",0], ["fives",0], ["sixes",0], ["bonus",0], ["twoPairs",0], ["threeOfAKind",0], ["fourOfAKind",0], ["smallStraight",0], ["largeStraight",0], ["fullHouse",0], ["yahtzee",0], ["chance",0]]
-    let diceSum = dice.reduce((partialSum, a) => partialSum + a, 0)
+    let diceSum = dice.map(({ value }) => value).reduce((partialSum, a) => partialSum + a, 0)
     let pairCount = 0
-    const diceThrowResult = {}
+    const diceThrowResults = new Map()
     //let diceCategoriesReceived = {aces:0, twos:0, threes:0, fours:0, fives:0, sixes:0, bonus:0, twoPairs:0, threeOfAKind:0, fourOfAKind:0, smallStraight:0, largeStraight:0, fullHouse:0, chance:0, yahtzee:0}
 
     diceValues.forEach (checkValues)
     function checkValues (value, index, array) {
+        diceCategoriesAndPoints[14][1] = diceSum                // Set points for Chance
         diceCategoriesAndPoints[index][1] = (index + 1) * value // Set values for upper section (aces, twos, threes, fours, fives and sixes)
         switch(value) {
             case 2:
@@ -105,7 +179,7 @@ function CheckThrowResults() {
         diceCategoriesAndPoints[7][1] = 0
     }
 
-    CheckForStraight(...dice.sort(function(a, b){return a - b}))    // Sort dice in ascending order and check for straight
+    CheckForStraight(...dice.map(({ value }) => value).sort(function(a, b){return a - b}))    // Get dice.values and sort them in ascending order and check for straight
     function CheckForStraight(a, b, c, d, e) {
         if (e == d+1 & d == c+1 & c == b+1 || d == c+1 & c == b+1 & b == a+1) {
             diceCategoriesAndPoints[10][1] = 30         // Set points for smallStraight
@@ -117,17 +191,20 @@ function CheckThrowResults() {
 
     CompileResult()                                     // Gather category options for return
     // TODO: Finish the following:
-/*     function CompileResult() {
-        const resultArray = []
-        const myArray = [a, b, c, d, e]
-        myArray.forEach(myFunction)
-        function myFunction(value, index, array) {
-            if (value > 0) {
-                resultArray.push(value)
+    function CompileResult() {
+        diceCategoriesAndPoints.forEach(CheckCategoryScore)
+        function CheckCategoryScore(value, index, array) {
+ /*            var tempObject = {}
+            tempObject[value[0]] = value[1]
+            if (value[1] > 0) {
+                diceThrowResults.push(tempObject)
+            } */
+            if (value[1] > 0) {
+                diceThrowResults.set(value[0], value[1])
             }
         }
-        return(resultArray)
-    } */
+    }
+    return(diceThrowResults)
 }
 
 function CheckForBonus() {
@@ -136,119 +213,10 @@ function CheckForBonus() {
     diceCategoriesAndPoints[2][1] +
     diceCategoriesAndPoints[3][1] +
     diceCategoriesAndPoints[4][1] +
-    diceCategoriesAndPoints[5][1] +) > 63) {
+    diceCategoriesAndPoints[5][1]) > 63) {
         scoreChart.bonus = 35
     }
 }
-
-/* function CalculatePoints() {
-    // Check dice values and calculate score
-    let tempScore = 0
-    let diceCombinationStacks = [0, 0, 0, 0, 0, 0]
-    let stackCount = 0
-    let stackValueCount = 0
-
-    diceValues.forEach (checkDice)
-    function checkDice (value, index, array) {
-        
-        if (value == 2) {
-            diceCombinationStacks[index] = diceCombinationStacks[index]+2
-        }
-        if (value == 3) {
-            diceCombinationStacks[index] = diceCombinationStacks[index]+3
-        }
-        if (value == 4) {
-            console.log("FOUR OF A KIND")
-            diceValues.forEach (checkForFourOfAKind)
-            function checkForFourOfAKind (value, index, array) {
-                if (value == 4) {
-                    tempScore = tempScore + ((index+1)*value)
-                    scoreChart[currentPlayer].fourOfAKind = tempScore
-                }
-            }
-        }
-        if (value == 5) {
-            console.log("YAHTZEEEE!!!!")
-            diceValues.forEach (checkForYahtzee)
-            function checkForYahtzee (value, index, array) {
-                if (value == 5) {
-                    tempScore = tempScore + ((index+1)*value)
-                    scoreChart[currentPlayer].yahtzee = tempScore
-                }
-            }
-        }
-      }
-
-      diceCombinationStacks.forEach (countStacks)
-      function countStacks (value, index, array) {
-          if (value == 2) {
-              stackCount = stackCount+1
-              stackValueCount = stackValueCount+((index+1)*value)
-          }
-          if (value == 3) {
-              stackCount = stackCount+3
-              stackValueCount = stackValueCount+((index+1)*value)
-          }
-      }
-
-    if (stackCount == 2) {
-        console.log("Two Pairs!")
-        tempScore = tempScore + stackValueCount
-        scoreChart[currentPlayer].twoPairs = tempScore
-    } else if (stackCount == 4) {
-        console.log("FULL HOUSE!")
-        tempScore = tempScore + stackValueCount
-        scoreChart[currentPlayer].fullHouse = tempScore
-    } else if (stackCount == 1) {
-        console.log("Pair")
-        diceValues.forEach (checkForPair)
-        function checkForPair (value, index, array) {
-            if (value == 2) {
-                tempScore = tempScore + ((index+1)*value)
-                scoreChart[currentPlayer].onePair = tempScore
-            }
-        }
-    } else if (stackCount == 3) {
-        console.log("Three of a kind")
-        diceValues.forEach (checkForThreeOfAKind)
-        function checkForThreeOfAKind (value, index, array) {
-            if (value == 3) {
-                tempScore = tempScore + ((index+1)*value)
-                scoreChart[currentPlayer].threeOfAKind = tempScore
-            }
-        }
-    }
-
-    if (dice[4] == dice[3]+1 & dice[3] == dice[2]+1 & dice[2] == dice[1]+1 & dice[1] == dice[0]+1) {
-        if (dice[4] == 5) {
-            console.log("Small Straight")
-            tempScore = 15
-            scoreChart[currentPlayer].smallStraight = tempScore
-        } else if (dice[4] == 6) {
-            console.log("Large Straight")
-            tempScore = 20
-            scoreChart[currentPlayer].largeStraight = tempScore
-        }
-    }
-
-    if (tempScore == 0) {
-        console.log("Oh, that's a shame...")
-    } else {
-        players[currentPlayer].totalScore = players[currentPlayer].totalScore + tempScore
-    }
-
-
-     switch(scoreCategory) {
-          case aces:
-            // code block
-            break;
-          case twos:
-            // code block
-            break;
-          default:
-            // code block
-        } 
-} */
 
 function ShowFinalScore() {
 
@@ -256,13 +224,15 @@ function ShowFinalScore() {
 
 
 
-//Only for testing - remove the fillowing
-/* playerNames.push("Josh", "Andrew", "Phil", "Steve", "Motown", "Funk")
+//Only for testing - remove following:
+/* numberOfPlayers = 6-1
+playerNames.push("Josh", "Andrew", "Phil", "Steve", "Motown", "Funk")
 SetupPlayerData()
-SetStartingPlayer()
-ThrowDice()
-console.table(dice)
-DiceArrayToValueArray(...dice)
-console.table(diceValues)
-//CalculatePoints()
-console.table(scoreChart) */
+let tryXThrows = 3
+for (let i = 0; i < tryXThrows; i++) {
+    ThrowDice()
+} */
+//console.table(dice)
+
+//console.table(scoreChart)
+//console.log(throwCount)
